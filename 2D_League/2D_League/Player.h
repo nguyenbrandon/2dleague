9f4e812a99626d5eb858874324259ccbdba24df1
nguyenbrandon <brando15@umbc.edu>
2016-10-07 01:30:01 -0400

    Add files via upload
    Brandon's version

--- 2D_League/2D_League/Player.h
+++ 2D_League/2D_League/Player.h
@@ -1,11 +1,19 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+enum roles { MAGE, MARKSMAN, SUPPORT, TANK, ASSASSIN, FIGHTER };
+enum strength { ZERO, VERY_WEAK, WEAK, AVERAGE, STRONG, VERY_STRONG };
+const string Roles[6] = { "Mage", "Marksman", "Support", "Tank", "Assassin", "Fighter" };
 
 class Player{
 public:
 
+	
+
 	Player(int selection);
 
+	int GetRole();
 	int GetTurrets();
 	void TurretDestroyed();
 
@@ -16,17 +24,31 @@ public:
 	void AddGold(int amount);
 	void SpendGold(int itemCost);
 
+	int GetCurrHP();
+	int GetMaxHP();
+	int GetPokeDmg();
+	int GetPokeResist();
+	int GetTurretDmg();
+	int GetAgility();
+
+	void IncCurrHP(int amount);
+	void IncMaxHP(int amount);
+	void IncPokeDmg(int amount);
+	void IncPokeResist(int amount);
+	void IncTurretDmg(int amount);
+	void IncAgility(int amount);
+
 private:
 	int m_Turrets;
 	bool m_Nexus;
 	int m_Gold;
-	int m_role;
+	int m_Role;
 
 	int m_CurrHP;
-	int m_MaxHp;
-	int pokeDmg;
-	int DmgResist;
-	int tStrength;
-	int agility;
+	int m_MaxHP;
+	int m_PokeDmg;
+	int m_PokeResist;
+	int m_TurretDmg;
+	int m_Agility;
 };
 
